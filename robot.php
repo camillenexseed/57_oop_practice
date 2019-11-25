454c41420d189e0f04ceb6c18c6d0cbc58f5519f
nexseedcamille <ykamimori@nexseed.net>
2019-11-25 15:05:49 +0800

    メソッド追加
    「Robot」クラスにメソッド追加

--- robot.php
+++ robot.php
@@ -8,4 +8,24 @@ class Robot
     // プロパティ追加
     public $name;
     public $food;
+
+    // メソッドの追加
+    // talkメソッド追加
+    public function talk()
+    {
+        echo 'こんにちは、ボクの名前は'.$this->name.'です<br>';
+    }
+
+    // eatメソッド追加
+    public function eat()
+    {
+        echo '好きな食べ物は'.$this->food.'です<br>';
+    }
+
+    // greetingメソッド追加
+    public function greeting()
+    {
+        $this->talk();
+        $this->eat();
+    }
 }
